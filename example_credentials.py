b5b68db894b145881af82f72830b13d47cf16062
jmausolf <jgm346@gmail.com>
2016-10-31 18:20:59 -0500

    Adding example credentials file

---
+++ example_credentials.py
@@ -0,0 +1,4 @@
+consumer_key = "yoursecretconsumerkey"
+consumer_secret = "yoursupersecret_consumer_secret"
+access_token_key = "your_access_token_key"
+access_token_secret = "your_access_token_secret"
