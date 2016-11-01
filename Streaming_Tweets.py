fa8726803a5090642884635a767ab877ab8dd699
jmausolf <jgm346@gmail.com>
2016-11-01 00:20:22 -0500

    Adding wrapper function and ability to pass keywords to streamer

--- Streaming_Tweets.py
+++ Streaming_Tweets.py
@@ -1,4 +1,4 @@
-import csv
+import csv, os, re
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
@@ -11,25 +11,34 @@ from mycredentials import *
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token_key, access_token_secret)
 
+#Utility Functions
+def remove_non_ascii_2(text):
+    return re.sub(r'[^\x00-\x7F]+', "'", text)
+
+def start_csv():
+    with open('streaming_results.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["DATE", "AUTHOR", "TWEET"])
+
+
+#Tweepy Streaming Listener
 class TwitterListener(StreamListener):
-
-    def start_csv():
-        with open('Clinton.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(["DATE", "AUTHOR", "TWEET"])
-
 
     def on_status(self, status):
         try:
-            with open('Clinton.csv', 'a') as f:
+            #Broken Attempt
+            #filename = str(self.keyword)+".csv"
+            #with open(filename, 'a') as f:
+            with open('streaming_results.csv', 'a') as f:
                 writer = csv.writer(f)
-                
+
                 #Tweet Content
                 author = str(status.author.screen_name.encode('utf-8'))
                 date = str(status.created_at)
-                tweet = str(status.text.encode('utf-8'))
+                tweet_raw = str(status.text.encode('utf-8'))
+                tweet = remove_non_ascii_2(tweet_raw)
                 
-                print(date, tweet)
+                print("{} Tweet: {} ...".format(date, tweet[0:50]))
                 writer.writerow([date, author, tweet])
 
         except Exception as error:
@@ -39,14 +48,29 @@ class TwitterListener(StreamListener):
         print(status_code)
 
 
-#So Still streams
-twitterStream = Stream(auth, TwitterListener())
-twitterStream.filter(track=['#Clinton'])
+#Wrapper Function
+def streaming_tweets(keyword):
+
+    filename = keyword.replace(" ", "").replace("#", "")+".csv"
+    print(filename)
+
+    try:
+        start_csv()
+        while True:
+            try:
+                #Initialize Tweepy Streamer
+                twitterStream = Stream(auth, TwitterListener())
+                twitterStream.filter(track=[keyword])
+            except Exception as error:
+                print(error)
+                print ("[*] saving results to {}".format(filename))
+                os.rename("streaming_results.csv", filename)
+
+    except KeyboardInterrupt:
+        print ("[*] User KeyboardInterrupt: Tweepy Streamer Haulted")
+        print ("[*] saving results to {}".format(filename))
+        os.rename("streaming_results.csv", filename)
 
 
-#while True:
-#    try:
-#        twitterStream = Stream(auth, TwitterListener())
-#        twitterStream.filter(track=['#Clinton'])
-#    except Exception as error:
-#        print(error)
+streaming_tweets("#ImWithHer")
+
